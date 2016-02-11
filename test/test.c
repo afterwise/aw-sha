71c63e102a445e141eea3c941041dc00936aec64
afterwise <afterwise@gmail.com>
2016-02-11 21:29:02 +0000

    Add test

---
+++ test/test.c
@@ -0,0 +1,41 @@
+
+#include "aw-sha1.h"
+#include <assert.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	(void) argc;
+	(void) argv;
+
+	const char data[] =
+		"Arvid Stjärnblom gick en Djurgårdsväg\n"
+		"fram – den, som kantad af almar med lutande\n"
+		"svarta stammar löper längs stranden af den\n"
+		"tysta Djurgårdsbrunnsviken, nedanför Skansens\n"
+		"skrofliga bergknallar. Utställningsområdet hade\n"
+		"han lämnat bakom sig.";
+	printf("%.*s\n", (int) sizeof data - 1, data);
+
+	unsigned char h[SHA1_SIZE];
+	char s[SHA1_SIZE * 2];
+	sha1(h, data, sizeof data - 1);
+	sha1str(s, h);
+	printf("%.*s\n", (int) sizeof s, s);
+
+	FILE *fd = fopen("test.tmp", "w");
+	fwrite(data, 1, sizeof data - 1, fd);
+	fclose(fd);
+
+	char buf[256];
+	fd = popen("shasum -a1 -b test.tmp", "r");
+	fgets(buf, sizeof buf, fd);
+	pclose(fd);
+	printf("%.*s\n", (int) strcspn(buf, " "), buf);
+
+	remove("test.tmp");
+	assert(strncmp(s, buf, sizeof s) == 0);
+
+	printf("OK\n");
+	return 0;
+}
+
